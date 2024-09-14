a5b6c9a3f3aeccaf439d5693857bee84cfc03468
armaghanasghar <armaghanwa@gmail.com>
2024-09-14 14:28:40 -0700

    Initial Commit

---
+++ SwiftulCrypto/SwiftulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulCryptoApp.swift
+//  SwiftulCrypto
+//
+//  Created by Armaghan Asghar on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
